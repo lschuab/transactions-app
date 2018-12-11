0e8718a3785272ff5e13a7ce5a78ad247ce25df4
Leandro Schuab <lschuab@gmail.com>
2018-12-11 11:59:55 -0700

    prepped for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,10 @@
+require('dotenv').load({path: './local.env'})
+
 module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "transactions_app",
+        database: "transactions_db",
         host: "localhost"
       },
       migrations: {
@@ -14,7 +16,13 @@ module.exports = {
     },
   production: {
       client: 'pg',
-      connection: process.env.DATABASE_URL,
+      connection: {
+        database: process.env.RDS_DB,
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT
+      },
       migrations: {
           directory: __dirname + '/db/migrations',
         },
